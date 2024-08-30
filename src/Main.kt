4ff7e339faa376e1793ba02d157599b1f12a69a9
Keith Shovan <shovakei000@warren.k12.in.us>
2024-08-30 12:09:15 -0400

    Inital

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val child = 5
+    val adult = 28
+    val senior = 87
+
+    val isMonday = true
+
+    println("The movie ticket price for a person aged $child is \$${ticketPrice(child, isMonday)}.")
+    println("The movie ticket price for a person aged $adult is \$${ticketPrice(adult, isMonday)}.")
+    println("The movie ticket price for a person aged $senior is \$${ticketPrice(senior, isMonday)}.")
+}
+
+fun ticketPrice(age: Int, isMonday: Boolean): Int {
+    return if (age <= 12) 15
+     else if (age <= 60 && age >= 13 && isMonday) 30
+    else if (age >= 61) 20
+    else -1
+    }
+}
